acbfce5f97cc0755946e137f8c77a57e4044b75c
Andy Tran <anhquoctran006@gmail.com>
2023-08-13 16:46:34 -0600

    Create .gitignore

---
+++ .next/server/app/chunks/ssr/[next]_entry_app-renderer_tsx_b1481b._.js
@@ -0,0 +1,28 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "chunks/ssr/[next]_entry_app-renderer_tsx_b1481b._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "chunks/ssr/[next]_entry_app-renderer_tsx_b1481b._.js",
+  "chunks": [
+    "chunks/ssr/_c5971f._.js",
+    "chunks/ssr/[next]_internal_operation-stream_ts_5d10d5._.js",
+    "chunks/ssr/[next]_internal_headers_ts_435e61._.js",
+    "chunks/ssr/[next]_internal_http_ts_a662b8._.js",
+    "chunks/ssr/[next]_polyfill_app-polyfills_ts_fa7482._.js",
+    "chunks/ssr/[next]_entry_app-renderer_tsx_911aa9._.js",
+    "chunks/ssr/[turbopack-node]_ipc_index_ts_603a02._.js",
+    "chunks/ssr/[turbopack-node]_compiled_stacktrace-parser_index_cd57ce.js",
+    "chunks/ssr/_b9f1d7._.js",
+    "chunks/ssr/[next]_entry_app-entry_tsx_4665ed._.js",
+    "chunks/ssr/[next]_entry_app-entry_tsx_c0e7e9._.js",
+    "chunks/ssr/[next]_entry_app_server-to-client-ssr_tsx_0b01bd._.js",
+    "chunks/ssr/[next]_entry_app_server-to-client-ssr_tsx_6467c7._.js",
+    "chunks/ssr/[next]_entry_app_server-to-client-ssr_tsx_cf64a1._.js",
+    "chunks/ssr/[next]_entry_app_server-to-client-ssr_tsx_d85798._.js",
+    "chunks/ssr/[next]_entry_app_server-to-client-ssr_tsx_5ab633._.js",
+    "chunks/ssr/[next]_entry_app_server-to-client-ssr_tsx_09f5a0._.js",
+    "chunks/rsc/app_globals.css"
+  ],
+  "source": "entry"
+});
